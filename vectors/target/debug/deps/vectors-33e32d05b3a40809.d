22a0dfff3366426e990339a1f9fc38eb7ac5b148
padames <pablo.adames@gmail.com>
2023-12-27 13:35:03 -0700

    more rust practice

---
+++ vectors/target/debug/deps/vectors-33e32d05b3a40809.d
@@ -0,0 +1,5 @@
+/home/pablo/git/rust/vectors/target/debug/deps/vectors-33e32d05b3a40809: src/main.rs
+
+/home/pablo/git/rust/vectors/target/debug/deps/vectors-33e32d05b3a40809.d: src/main.rs
+
+src/main.rs:
---
+++ vectors/target/debug/vectors.d
@@ -0,0 +1,1 @@
+/home/pablo/git/rust/vectors/target/debug/vectors: /home/pablo/git/rust/vectors/src/main.rs
