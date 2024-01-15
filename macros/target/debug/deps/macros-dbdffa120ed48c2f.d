997700f6af914a517093ef3df9c6327ad1ceb142
padames <pablo.adames@gmail.com>
2024-01-14 21:32:06 -0700

    adding learning code to create macros

---
+++ macros/target/debug/deps/macros-dbdffa120ed48c2f.d
@@ -0,0 +1,5 @@
+/home/pablo/git/rust/macros/target/debug/deps/macros-dbdffa120ed48c2f: src/main.rs
+
+/home/pablo/git/rust/macros/target/debug/deps/macros-dbdffa120ed48c2f.d: src/main.rs
+
+src/main.rs:
---
+++ macros/target/debug/macros.d
@@ -0,0 +1,1 @@
+/home/pablo/git/rust/macros/target/debug/macros: /home/pablo/git/rust/macros/src/main.rs
